4938b22eaafc55ab2209744c8409629b56ee2efa
Sangki Jun <sangki810@gmail.com>
2026-05-29 14:45:53 -0400

    Fix spelling error in About section
    Corrected the spelling of 'University' in the About section.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -14,7 +14,7 @@ const About = () => {
       </h1>
       <div>
         <img className='pt-4' src={Photo} style={photoStyle}></img>
-        <h4 className='pt-4'>I am a 1.5 generation Korean-American who moved to the states in 2006 from South Korea. I received all primary education in Gainesville, Florida and graduted from the Univserity of Florida in 2020. Currently I am taking the UCF FSF Coding Bootcamp and learning the various full-stack developer languages to add to my skill set.</h4>
+        <h4 className='pt-4'>I am a 1.5 generation Korean-American who moved to the states in 2006 from South Korea. I received all primary education in Gainesville, Florida and graduted from the University of Florida in 2020. Currently I am taking the UCF FSF Coding Bootcamp and learning the various full-stack developer languages to add to my skill set.</h4>
       </div>
     </div>
   );
